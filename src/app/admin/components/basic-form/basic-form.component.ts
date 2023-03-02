a409df566b7d47459647abbf18b6c20e91987b7c
miltondw <estradamilton2001@gmail.com>
2023-03-02 15:33:56 -0500

    Commit changes to the Basic Form component to update the form elements and add form validation

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,47 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
   styleUrls: ['./basic-form.component.scss']
 })
-export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl()
-  categories = new FormControl();
-  myGroup: FormGroup;
-  categoriesList: string[] = ['Tech', 'Fashion', 'TV', 'Smartphone', 'shoes', 'others'];
-  constructor () {
-    this.myGroup = new FormGroup({
-      phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(15)])
-    });
-  }
-  ngOnInit(): void {
-    this.nameField.valueChanges.subscribe(v => {
-      console.log(v, "Reactive time")
-    })
-  }
-  getName() {
-    console.log(this.nameField.value, "value fieldName")
-    console.log(this.nameField, "value field")
-  }
+export class BasicFormComponent {
 
-  getErrorMessage() {
-    switch (this.myGroup.get('phone')?.touched) {
-      case this.myGroup.get('phone')?.hasError('required'):
-        return "Your Phone is required"
+  categoriesList: string[] = ['Tech', 'Fashion', 'TV', 'Smartphone', 'shoes', 'others']
+
+  constructor (private fb: FormBuilder) { }
+
+  form = this.fb.group({
+    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(15)]],
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    categories: [[""]]
+  });
+  requiredLength: number = 0
+  getErrorMessage(value: string) {
+    switch (true) {
+      case this.form.get(value)?.hasError('required'):
+        return `Your ${value} is required`
         break;
-      case this.myGroup.get('phone')?.hasError('minlength'):
-        return "Min length is 10"
-        break;
-      case this.myGroup.get('phone')?.hasError('maxlength'):
-        return "Max length is 15"
+      case this.form.get(value)?.hasError('minlength'):
+        this.requiredLength = this.form.get(value)?.getError('minlength')?.requiredLength
+        return `Min length ${value} is ${this.requiredLength}`
+      case this.form.get(value)?.hasError('maxlength'):
+        this.requiredLength = this.form.get(value)?.getError('maxlength')?.requiredLength
+        return `Max length ${value} is ${this.requiredLength}`
         break;
       default:
-        return ""
+        return null
         break;
     }
   }
-
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched()
+    }
+  }
+  invalidValue(value:string) {
+    return this.form.get(value)?.touched && this.form.get(value)?.invalid
+  }
+  get invalidName() {
+    return this.name?.touched && this.name?.invalid
+  }
+  get name() {
+    return this.form.get('name')
+  }
+  get phone() {
+    return this.form.get('phone')
+  } get categories() {
+    return this.form.get('categories')
+  }
 }
