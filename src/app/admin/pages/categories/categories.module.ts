7996956f824ee846a9eb1dce2f84412eecd7e5de
miltondw <estradamilton2001@gmail.com>
2023-03-04 14:06:28 -0500

    Commit changes to categories module and components to add Material modules and update and delete methods

--- src/app/admin/pages/categories/categories.module.ts
+++ src/app/admin/pages/categories/categories.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
+//Material
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+//Components
 import { CategoriesComponent } from './components/categories/categories.component';
 import { FormCategoriesComponent } from './components/form-categories/form-categories.component';
+
 
 
 @NgModule({
@@ -13,7 +21,12 @@ import { FormCategoriesComponent } from './components/form-categories/form-categ
   ],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CategoriesModule { }
--- src/app/admin/pages/categories/components/categories/categories.component.ts
+++ src/app/admin/pages/categories/components/categories/categories.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from '@app/models/Product';
+import { CategoryService } from '@app/services/category.service'
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'typeImg','update','delete'];
+  dataSource:ICategory[] =[];
+  constructor (private categoryService: CategoryService) { }
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.dataSource = data
+      console.log(this.dataSource)
+    })
+  }
+  deleteCategory(id:number){
+    console.log(id)
+  }
+  updateCategory(id:number){
+    console.log(id)
+  }
 }
